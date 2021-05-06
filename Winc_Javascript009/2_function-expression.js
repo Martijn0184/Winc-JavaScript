1cea76a42b4d55ab7882619e44b113c7719bb8ca
Martijn den Breejen <60103411+Martijn0184@users.noreply.github.com>
2021-05-06 11:51:04 +0200

    Add files via upload

---
+++ Winc_Javascript009/2_function-expression.js
@@ -0,0 +1,9 @@
+const fE = function (num1, num2) {
+    const num3 = num1 * num1;
+    const num4 = num2 * num2;
+    const num5 = num3 + num4;
+    const num6 = num5 * num5;
+    return console.log(num6);
+};
+
+fE(5, 6);
---
+++ Winc_Javascript009/3_arrow-function.js
@@ -0,0 +1,9 @@
+const aF = (num1, num2) => {
+    const num3 = num1 * num1;
+    const num4 = num2 * num2;
+    const num5 = num3 + num4;
+    const num6 = num5 * num5;
+    return console.log(num6);
+};
+
+aF(5, 6);
