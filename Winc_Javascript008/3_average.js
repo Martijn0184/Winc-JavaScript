fd24df095312af90343e7e2396935d06360703b3
Martijn den Breejen <60103411+Martijn0184@users.noreply.github.com>
2021-05-11 11:15:37 +0200

    Delete Winc_Javascript008 directory

--- Winc_Javascript008/3_average.js
+++
@@ -1,9 +0,0 @@
-function calcAverage(arr) {
-    var numbersCnt = arr.length;
-    var allNum = arr.reduce(function (total, arr) { return total + arr }, 0);
-    return averageNum = (allNum / numbersCnt);
-    math.round(averageNum);
-}
-
-calcAverage(arr = [5, 25, 20, 25, 25]);
-console.log(averageNum);
