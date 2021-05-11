fde6064fce5b07c79e3b51b4d778d839d3973e3a
Martijn den Breejen <60103411+Martijn0184@users.noreply.github.com>
2021-05-11 11:15:48 +0200

    Delete Winc_Javascript009 directory

--- Winc_Javascript009/1_function-declaration.js
+++
@@ -1,9 +0,0 @@
-function fD(num1, num2) {
-    const num3 = num1 * num1;
-    const num4 = num2 * num2;
-    const num5 = num3 + num4;
-    const num6 = num5 * num5;
-    return console.log(num6);
-}
-
-fD(5, 6);
